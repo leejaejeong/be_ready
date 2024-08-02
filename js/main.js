7840bcd135059069fd393bcee6a6dc223646c0e8
leejaejeong <jaejeonglee6@gmail.com>
2024-08-02 12:52:43 +0900

    메인, 서브 프로젝트 마크업 및 기능 작업

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
+    $(function(){
+        $("html, body").animate({ scrollTop: 0 }, "fast"); 
+    });
+    
+ 
+    $('body').addClass('stop-scroll');
+    setTimeout(function(){
+        $('body').removeClass('stop-scroll');
+    },5500);
+
+
     setInterval(function(){
         let now = new Date();
         let hr = now.getHours();
         let min = now.getMinutes();
-        $('.current_time').text(hr + ':' + min);
+        let amPm = now.getHours() < 12 ? "AM" : "PM";
+        $('.current_time').text(hr + ':' + min + ' ' + amPm);
         if(hr < 10){
             $('.current_time').text('0' + hr + ':' + min); 
         }
@@ -13,6 +25,6 @@ $(document).ready(function(){
     },1000);
 
     let header = gsap.timeline();
-    header.from('.header', { y:-150, duration: 1, delay: 3.5 });
+    header.from('.header', { y:-150, duration: 1, delay: 4 });
 
 });
